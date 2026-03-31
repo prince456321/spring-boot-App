f46a74631cb46debe80b775711d490935b717f1e
tete <jojo@example.com>
2026-03-31 23:15:48 +0200

    Update workflows

---
+++ terraform-aws-music-app/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-demo-music"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table" # Optionnel mais recommandé
+  }
+}
+
