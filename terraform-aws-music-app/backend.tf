e02a95a4e70bc8b30664e78b93072d58908379dd
prince456321 <141109052+prince456321@users.noreply.github.com>
2026-03-31 23:26:23 +0200

    Update backend.tf

--- terraform-aws-music-app/backend.tf
+++ terraform-aws-music-app/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-demo-music"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table" # Optionnel mais recommandé
+    bucket = "s3-demo-music"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "terraform-lock-table"  <-- Supprimé ou commenté
   }
 }
-
